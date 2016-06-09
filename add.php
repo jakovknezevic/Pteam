3fa8da96841e21e4e827538a7a346a009cdf788c
Jakov Knežević <jakov.knezevic@skole.hr>
2016-06-09 12:02:37 +0200

    Jel radis??

---
+++ add.php
@@ -0,0 +1,25 @@
+<?php require('check.php'); ?>
+<?php require('db.php'); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Automobili</title>
+</head>
+
+<body>
+<h1>
+    Automobili
+</h1>
+
+<p>
+    <?php
+    $sql = "INSERT INTO korisnici (gameId) VALUES ('" . $_POST['registracija'] . "', '" . $_POST['ime'] . "')";
+    $result = $conn->query($sql);
+
+    echo('Automobil je unesen!<br>');
+    echo('<a href="index.php">Povratak na prikaz automobila</a>');
+    ?>
+</p>
+</body>
+</html>
---
+++ login_check.php
@@ -0,0 +1,31 @@
+
+
+<?php require('db.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>666</title>
+</head>
+
+<body>
+<h1>
+    420 Blaze it
+</h1>
+
+<p>
+    <?php
+    $sql = "SELECT * FROM korisnici WHERE username='" . $_POST['username'] . "' AND password='" . $_POST['password'] . "'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        session_start();
+        $_SESSION['id'] = $row['id'];
+        header('Location: index2.php');
+    } else {
+        header('Location: login.php?login=fail');
+    }
+    ?>
+</p>
+</body>
+</html>
